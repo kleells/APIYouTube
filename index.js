2853f7e2711a631cd15d8dfc13c7badfef6d7b26
Katie Eells <katieeells@Katies-MBP.lan>
2023-03-01 20:09:24 -0500

    build API YouTube search query in JS

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const apiKey = 'AIzaSyAnlUeip6LikHAXt30Dy8EGj-SUebwQVTM';
+const searchButton = document.getElementById('searchButton');
+const googleAPIUrl = "https://www.googleapis.com/youtube/v3/search"
+
+searchButton.addEventListener('click', (e) => {
+    // Build a YouTube API query (more info on Google API documentation)
+    const searchTerm = document.getElementById('searchTerm').value;
+
+    const apiPrefix = '&key=';
+    const searchQuery = '?part=snippet&q=' + searchTerm + apiPrefix + apiKey;
+    const url = googleAPIUrl + searchQuery;
+
+    fetch(url);
+});
